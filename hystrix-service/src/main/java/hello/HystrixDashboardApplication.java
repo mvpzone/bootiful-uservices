c02f2c65bc03051e64240e63e0484617781681e0
Vinesh Prasanna M <741788+mvpzone@users.noreply.github.com>
2017-12-02 21:37:37 -0800

    Added zipkin support to zuul-service.

--- hystrix-service/src/main/java/hello/HystrixDashboardApplication.java
+++ hystrix-service/src/main/java/hello/HystrixDashboardApplication.java
@@ -3,12 +3,20 @@ package hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @EnableHystrixDashboard
 @SpringBootApplication
+@Controller
 public class HystrixDashboardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HystrixDashboardApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "forward:/hystrix";
 	}
 }
--- zuul-service/src/main/java/com/example/zuulservice/ZuulServiceApplication.java
+++ zuul-service/src/main/java/com/example/zuulservice/ZuulServiceApplication.java
@@ -20,6 +20,7 @@ public class ZuulServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulServiceApplication.class, args);
 	}
+
 }
 
 @Slf4j
